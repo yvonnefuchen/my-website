b54d1a91d7812b11b394d1a449c447ce4aeb07ce
David Chen <davidlchen@gmail.com>
2025-08-04 14:16:50 +0800

    remove z position for the footer to it doesn't appear above the projects cards

--- starter-template/src/sections/Footer.tsx
+++ starter-template/src/sections/Footer.tsx
@@ -14,7 +14,7 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer className="relative z-10 overflow-x-clip">
+    <footer className="relative overflow-x-clip">
       <div className="absolute h-[250px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container pb-4">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
